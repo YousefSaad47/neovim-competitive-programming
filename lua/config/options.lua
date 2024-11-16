96b44735fceaaae41bd9252ab2f9eff1242d8d24
yousef <YoussefSaad299@gmail.com>
2024-11-16 22:28:09 +0200

    Add additional options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.timeoutlen = 300
+vim.opt.updatetime = 100
+vim.opt.clipboard = "unnamedplus"
+vim.opt.wrap = true
